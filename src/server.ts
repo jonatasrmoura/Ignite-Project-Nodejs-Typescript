2c68e3538bdc7356e08eb9ddc50dbff83e4b84c9
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2021-12-05 17:31:05 -0300

    fix#Project Node.js to Typescript

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  
-})
+  return response.json({ message: "Hello World Ignite" });
+});
 
 app.listen(3333);
