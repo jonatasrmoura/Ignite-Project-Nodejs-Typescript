cef60339df5f64872654d6f6c5da5a4b4cb1dc8c
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2021-12-05 20:08:41 -0300

    feat#configurando ambiente de desevolvimento

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { CreateCourse } from "./routes";
 
 const app = express();
 
-app.get("/", CreateCourse);
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World!" })
+});
 
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running"));
