604df7ea83c9f88cc0d832f4a8189bbc3892104d
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2021-12-14 20:44:01 -0300

    feat#Refatorando a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/listCategoriesUseCase.ts
@@ -0,0 +1,14 @@
+import { Category } from "../../model/Category";
+import { IcategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: IcategoriesRepository) {}
+
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
