b1d56475c27f9df5381a63adafab67885c4b729d
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2021-12-15 22:31:43 -0300

    feat#Conhecendo o conceito de stream

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
 
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,17 @@
-
+import { parse as csvParse } from 'csv-parse';
+import fs from 'fs';
 
 class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
 
-  execute(file: any) {
-    console.log(file);
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
