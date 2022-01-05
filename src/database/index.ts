952d1e30341e574b4ca7f80384632cc6f4aff1c5
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-01-04 23:18:31 -0300

    feat#Criando container do postgres

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,13 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+interface IOption {
+  host: string;
+}
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOption;
+  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
