d8f6e01161241f1e869282abcc4c5426509bd8ff
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-01-29 18:18:41 -0300

    feat#Criando token do usuario

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,60 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  },
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // Usuário existe, se sim...
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or Password incorrect!");
+    }
+
+    // ...Senha está correta, se sim...
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or Password incorrect!");
+    }
+
+    // ...Gerar jsonwebtoken
+    const token = sign({}, "f57208dc433b16fc5d685fef83001e80", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
-import { categoriesRoutes } from '../routes/categories.routes';
-import { specificationsRoutes } from "../routes/specifications.routes";
-import { usersRoutes } from "../routes/users.routes";
+import { categoriesRoutes } from './categories.routes';
+import { specificationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
+import { authenticateRoutes } from "./authenticate.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
