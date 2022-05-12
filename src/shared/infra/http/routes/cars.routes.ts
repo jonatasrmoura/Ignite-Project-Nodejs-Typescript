360b7c572d748e4befa5ca3227c64fd3081ba540
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-05-12 20:21:24 -0300

    feat#Filtro de listagem de carros disponiveis

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
   "/",
@@ -14,5 +16,7 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
